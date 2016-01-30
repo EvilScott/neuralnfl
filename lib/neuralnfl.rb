53a77c724d5769f5a3732a927d3423fe76480ebe
EvilScott <reis.robert.s@gmail.com>
2016-01-30 15:20:58 -0800

    create helper methd on project module

--- lib/neuralnfl.rb
+++ lib/neuralnfl.rb
@@ -5,4 +5,11 @@ require_relative 'neural_nfl/network'
 require_relative 'neural_nfl/layer'
 require_relative 'neural_nfl/neuron'
 
-module NeuralNFL; end
+module NeuralNFL
+  class << self
+    def create(input_count, output_count)
+      hidden_count = (input_count + output_count) / 2
+      NeuralNFL::Network.new(input_count, hidden_count, output_count, 0.25)
+    end
+  end
+end
