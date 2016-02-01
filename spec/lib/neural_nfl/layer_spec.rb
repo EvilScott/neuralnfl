a3e1f244c4013a26da8e9c547f2d8d0edfac2d6c
EvilScott <reis.robert.s@gmail.com>
2016-01-31 17:58:04 -0800

    fill in some layer tests

--- spec/lib/neural_nfl/layer_spec.rb
+++ spec/lib/neural_nfl/layer_spec.rb
@@ -3,11 +3,10 @@ require_relative '../../spec_helper'
 module NeuralNFL
   describe Layer do
     let(:test_layer) do
-      test_layer = Layer.new(3, 2)
+      test_layer = Layer.new(2, 2)
       test_layer.nodes = [
-          instance_double('Neuron', eval: 0.0, out: 0.0),
-          instance_double('Neuron', eval: 0.5, out: 0.5),
-          instance_double('Neuron', eval: 1.0, out: 1.0)
+          instance_double(Neuron),
+          instance_double(Neuron)
       ]
       test_layer
     end
@@ -23,13 +22,17 @@ module NeuralNFL
 
     describe '#eval' do
       it 'evaluates a layer of neurons and returns an array of outputs' do
-        expect(test_layer.eval(nil)).to eq [0.0, 0.5, 1.0]
+        expect(test_layer.nodes[0]).to receive(:eval).and_return(0.25)
+        expect(test_layer.nodes[1]).to receive(:eval).and_return(0.5)
+        expect(test_layer.eval([0.25, 0.5])).to eq [0.25, 0.5]
       end
     end
 
     describe '#output_deltas' do
       it 'calculates deltas for an output layer' do
-        expect(test_layer.output_deltas([0.0, -0.5, 1.0])).to eq [0.0, -0.25, 0.0]
+        expect(test_layer.nodes[0]).to receive(:out).exactly(3).times.and_return(0.5)
+        expect(test_layer.nodes[1]).to receive(:out).exactly(3).times.and_return(0.5)
+        expect(test_layer.output_deltas([1, 1])).to eq [0.125, 0.125]
       end
     end
 
