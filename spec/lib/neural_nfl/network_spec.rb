f58af92170a55ed9113e03f4f703de906ad6160c
EvilScott <reis.robert.s@gmail.com>
2016-01-31 18:30:52 -0800

    finish out the tests

--- spec/lib/neural_nfl/network_spec.rb
+++ spec/lib/neural_nfl/network_spec.rb
@@ -2,28 +2,34 @@ require_relative '../../spec_helper'
 
 module NeuralNFL
   describe Network do
+    let(:test_network) { Network.new(1, 1, 1, 0.25) }
+
     describe '#eval' do
-      pending
-      # it 'returns the output of the network given a set of inputs' do
-      # end
+      it 'returns the output of the network given a set of inputs' do
+        expect(test_network.hidden_layer).to receive(:eval).with([0.5]).and_return([0.25])
+        expect(test_network.output_layer).to receive(:eval).with([0.25]).and_return([0.75])
+        expect(test_network.eval([0.5])).to eq [0.75]
+      end
     end
 
     describe '#train!' do
-      pending
-      # it 'trains the network' do
-      # end
+      it 'trains the network' do
+        output_nodes = test_network.output_layer.nodes
+        expect(test_network).to receive(:eval).and_return([0.5])
+        expect(test_network.output_layer).to receive(:output_deltas).with([0.5]).and_return([0.25])
+        expect(test_network.hidden_layer).to receive(:hidden_deltas).with(output_nodes, [0.25]).and_return([0.5])
+        expect(test_network.output_layer).to receive(:update_weights!).with([0.25], 0.25).and_return([0.25])
+        expect(test_network.hidden_layer).to receive(:update_weights!).with([0.5], 0.25).and_return([0.5])
+        test_network.train!([0.5], [0.5])
+      end
     end
 
     describe '#serialize' do
-      pending
-      # it 'saves the network' do
-      # end
+      pending 'TODO implementation'
     end
 
     describe '.from_serialized' do
-      pending
-      # it 'restores the network' do
-      # end
+      pending 'TODO implementation'
     end
   end
 end
