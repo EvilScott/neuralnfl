6b8f12a1cae015081b25e135e4cbcb26786030a1
EvilScott <reis.robert.s@gmail.com>
2016-02-02 08:57:07 -0800

    start filling in tests

--- spec/lib/neural_nfl/network_spec.rb
+++ spec/lib/neural_nfl/network_spec.rb
@@ -2,6 +2,8 @@ require_relative '../../spec_helper'
 
 module NeuralNFL
   describe Network do
+    let (:net) { Network.new(1, 3, 2, 0.25) }
+
     describe '#evaluate' do
       pending
     end
@@ -11,7 +13,11 @@ module NeuralNFL
     end
 
     describe '#get_output_deltas' do
-      pending
+      it 'provides deltas for the output layer' do
+        expected = [1.0, 0.0]
+        output = [0.5, 0.25]
+        expect(net.get_output_deltas(expected, output)).to eq [0.125, -0.046875]
+      end
     end
 
     describe '#get_hidden_deltas' do
