fe76382bc477928987be7606b1af3d12ec379238
EvilScott <reis.robert.s@gmail.com>
2016-02-02 08:56:55 -0800

    add some todos

--- lib/neural_nfl/network.rb
+++ lib/neural_nfl/network.rb
@@ -2,6 +2,11 @@ module NeuralNFL
   class Network
     attr_accessor :hidden_layer, :output_layer
 
+    #TODO bias
+    #TODO momentum
+    #TODO dropout
+    #TODO multiple hidden layers
+
     def initialize(input_count, hidden_count, output_count, learning_rate)
       @layers = [
           Array.new(hidden_count, Vector.elements(Array.new(input_count, rand(-1.0..1.0)))),
@@ -12,6 +17,7 @@ module NeuralNFL
       @activate_prime = lambda { |x| x * (1 - x) }
     end
 
+    #TODO dry this
     def evaluate(inputs)
       @layers.reduce(Vector.elements(inputs)) do |input, layer|
         Vector.elements(layer.map { |node| @activate.call(node.inner_product(input)) })
@@ -20,6 +26,7 @@ module NeuralNFL
 
     def train(inputs, expected)
       inputs = Vector.elements(inputs)
+      #TODO dry this
       hidden_outs = Vector.elements(@layers.first.map { |node| @activate.call(node.inner_product(inputs)) })
       output_outs = Vector.elements(@layers.last.map { |node| @activate.call(node.inner_product(hidden_outs)) })
       output_deltas = get_output_deltas(expected, output_outs)
@@ -41,6 +48,7 @@ module NeuralNFL
       end
     end
 
+    #TODO dry this
     def update_output_weights!(inputs, deltas)
       @layers[1] = @layers.last.each_with_index.map do |weights, node_index|
         weights = weights.each_with_index.map do |weight, weight_index|
