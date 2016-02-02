4369cee2902b23e14b11d69f5f72328d87fc99f8
EvilScott <reis.robert.s@gmail.com>
2016-02-02 08:49:11 -0800

    get it working

--- lib/neural_nfl/network.rb
+++ lib/neural_nfl/network.rb
@@ -8,18 +8,20 @@ module NeuralNFL
           Array.new(output_count, Vector.elements(Array.new(hidden_count, rand(-1.0..1.0))))
       ]
       @learning_rate = learning_rate
+      @activate = lambda { |x| 1 / (1 + Math.exp(-1 * x )) }
+      @activate_prime = lambda { |x| x * (1 - x) }
     end
 
     def evaluate(inputs)
       @layers.reduce(Vector.elements(inputs)) do |input, layer|
-        Vector.elements(layer.map { |weights| weights.inner_product(input) })
+        Vector.elements(layer.map { |node| @activate.call(node.inner_product(input)) })
       end
     end
 
     def train(inputs, expected)
       inputs = Vector.elements(inputs)
-      hidden_outs = Vector.elements(@layers.first.map { |weights| weights.inner_product(inputs) })
-      output_outs = Vector.elements(@layers.last.map { |weights| weights.inner_product(hidden_outs) })
+      hidden_outs = Vector.elements(@layers.first.map { |node| @activate.call(node.inner_product(inputs)) })
+      output_outs = Vector.elements(@layers.last.map { |node| @activate.call(node.inner_product(hidden_outs)) })
       output_deltas = get_output_deltas(expected, output_outs)
       hidden_deltas = get_hidden_deltas(hidden_outs, @layers.last, output_deltas)
       update_output_weights!(hidden_outs, output_deltas)
@@ -28,30 +30,32 @@ module NeuralNFL
 
     def get_output_deltas(expected, output)
       expected.zip(output.to_a).map do |expect, out|
-        (expect - out) * out * (1 - out)
+        (expect - out) * @activate_prime.call(out)
       end
     end
 
     def get_hidden_deltas(hidden_outs, output_nodes, output_deltas)
       @layers.first.each_index.map do |node_index|
         error = output_nodes.zip(output_deltas).map { |weights, delta| weights[node_index] * delta }.reduce(:+)
-        error * hidden_outs[node_index] * (1 - hidden_outs[node_index])
+        error * @activate_prime.call(hidden_outs[node_index])
       end
     end
 
     def update_output_weights!(inputs, deltas)
-      @layers.last.each_with_index do |weights, node_index|
-        weights.each_with_index do |weight, weight_index|
-          weight - (@learning_rate * inputs[weight_index] * deltas[node_index])
+      @layers[1] = @layers.last.each_with_index.map do |weights, node_index|
+        weights = weights.each_with_index.map do |weight, weight_index|
+          weight + (@learning_rate * inputs[weight_index] * deltas[node_index])
         end
+        Vector.elements(weights)
       end
     end
 
     def update_hidden_weights!(inputs, deltas)
-      @layers.first.each_with_index do |weights, node_index|
-        weights.each_with_index do |weight, weight_index|
-          weight - (@learning_rate * inputs[weight_index] * deltas[node_index])
+      @layers[0] = @layers.first.each_with_index.map do |weights, node_index|
+        weights = weights.each_with_index.map do |weight, weight_index|
+          weight + (@learning_rate * inputs[weight_index] * deltas[node_index])
         end
+        Vector.elements(weights)
       end
     end
 
--- spec/lib/neural_nfl/network_spec.rb
+++ spec/lib/neural_nfl/network_spec.rb
@@ -2,38 +2,28 @@ require_relative '../../spec_helper'
 
 module NeuralNFL
   describe Network do
-    let(:test_network) { Network.new(1, 1, 1, 0.25) }
-
     describe '#evaluate' do
-      it 'returns the output of the network given a set of inputs' do
-        expect(test_network.hidden_layer).to receive(:evaluate).with([0.5]).and_return([0.25])
-        expect(test_network.output_layer).to receive(:evaluate).with([0.25]).and_return([0.75])
-        expect(test_network.evaluate([0.5])).to eq [0.75]
-      end
+      pending
     end
 
-    describe '#train!' do
-      it 'trains the network' do
-        output_nodes = test_network.output_layer.nodes
-        expect(test_network).to receive(:evaluate).and_return([0.5])
-        expect(test_network.output_layer).to receive(:output_deltas).with([0.5]).and_return([0.25])
-        expect(test_network.hidden_layer).to receive(:hidden_deltas).with(output_nodes, [0.25]).and_return([0.5])
-        expect(test_network.output_layer).to receive(:update_weights!).with([0.25], 0.25).and_return([0.25])
-        expect(test_network.hidden_layer).to receive(:update_weights!).with([0.5], 0.25).and_return([0.5])
-        test_network.train!([0.5], [0.5])
-      end
+    describe '#train' do
+      pending
     end
 
-    describe '#serialize' do
-      it 'serializes the network into YAML' do
-        expect(test_network.serialize).to be_a String
-      end
+    describe '#get_output_deltas' do
+      pending
     end
 
-    describe '.from_serialized' do
-      it 'returns a network from serialized YAML' do
-        expect(Network.from_serialized(test_network.serialize)).to be_an_instance_of Network
-      end
+    describe '#get_hidden_deltas' do
+      pending
+    end
+
+    describe '#update_output_weights!' do
+      pending
+    end
+
+    describe '#update_hidden_weights!' do
+      pending
     end
   end
 end
