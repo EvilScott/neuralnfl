29c038026eaf3fdfc8b20df76d2bfd66fa99fd3b
EvilScott <reis.robert.s@gmail.com>
2016-02-04 08:41:53 -0800

    add sin example to committed code and make front more focused on NFL usage

--- lib/neuralnfl.rb
+++ lib/neuralnfl.rb
@@ -4,12 +4,17 @@
 require_relative 'neural_nfl/network'
 require 'matrix'
 require 'yaml'
+require 'csv'
 
 module NeuralNFL
   class << self
-    def create(input_count, output_count)
-      hidden_count = (input_count + output_count) / 2
-      NeuralNFL::Network.new(input_count, hidden_count, output_count, 0.25)
+    def network
+      unless @network
+        training_data = CSV.read('data/testdata.csv').map { |row| row.map(&:to_f) }
+        @network = NeuralNFL::Network.new(5, 7, 8, 0.25)
+        training_data.each { |datum| @network.train!(datum.first(5), datum.last(8)) }
+      end
+      @network
     end
   end
 end
