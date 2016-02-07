1644bba02035d17c788db7b1d81fce38ec2b3c06
EvilScott <reis.robert.s@gmail.com>
2016-02-07 12:22:19 -0800

    use jquery for form submits

--- neuralnfl.rb
+++ neuralnfl.rb
@@ -1,13 +1,14 @@
 # Neural NFL
 # Robert Scott Reis 2016
 
+require 'json'
 require 'weka'
 require 'sinatra/base'
 require 'haml'
 
 PLAYS_TYPES = ['Run', 'Pass', 'Field goal', 'Fake field goal', 'Punt', 'Fake punt', 'Kneel', 'Spike']
 
-instances = Weka::Core::Instances.from_arff('nfl.arff')
+instances = Weka::Core::Instances.from_arff('data/nfl.arff')
 instances.class_attribute = :type
 $classifier = Weka::Classifiers::Bayes::BayesNet.new
 $classifier.train_with_instances(instances)
@@ -18,13 +19,18 @@ class NeuralNFL < Sinatra::Base
   end
 
   post '/' do
+    halt(400) if params.any? { |_, v| v.to_s.empty? }
+
     time = (60.0 - (params['quarter'].to_i * 15.0) + params['time'].to_i) / 60.0
-    down = (params['down'].to_i / 4.0).to_s.to_sym
+    down = params['down'].to_sym
     to_first = params['to-first'].to_i / 100.0
     to_goal = params['to-goal'].to_i / 100.0
     score_diff = params['off-score'].to_i - params['def-score'].to_i
-    @results = $classifier.distribution_for [time, down, to_first, to_goal, score_diff, '?']
-    haml :index
+    $classifier
+        .distribution_for([time, down, to_first, to_goal, score_diff, '?'])
+        .map { |k, v| [k, v.round(2)] }
+        .sort { |x, y| y.last <=> x.last }
+        .to_json
   end
 
   run! if app_file == $0
--- public/neuralnfl.js
+++ public/neuralnfl.js
@@ -1,6 +1,23 @@
 NeuralNFL = {
     init: function() {
-        //TODO
+        $('form#inputs').submit(function(e) {
+            e.preventDefault();
+
+            $.ajax({
+                url: '/',
+                method: 'post',
+                data: $('form#inputs').serializeArray().reduce(function(values, field) {
+                    values[field.name] = field.value;
+                    return values;
+                }, {}),
+                error: function() {
+                    alert('incomplete form');
+                },
+                success: function(res) {
+                    console.log(res);
+                }
+            });
+        });
     }
 };
 
